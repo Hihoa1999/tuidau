e2fa5ba169bdce45d6f81c7789cf8c186cce4353
Nguyen Chi Hoa <58041946+Hihoa1999@users.noreply.github.com>
2020-02-25 10:46:55 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+	<link href="sty.css" rel="stylesheet" type="text/css">
+	<link rel="shortcut icon" type="img/png" href="smiling-emoticon-icon-cute-emoji-pig-vector-22501480.jpg">
+	<link rel="stylesheet" href="Framework/animate.css-master/animate.min.css" type="text/css">
+	<script type="text/javascript" src="Framework/jquery-3.4.1.min.map"></script>
+	<link rel="stylesheet" href="text.js" type="text/javascript">
+	<script type="text/javascript">
+	</script>
+</head>
+
+<body>
+	<div class="pic1 hide">
+		<div class="text1">
+			<span class="bor tran animated rotateInUpRight delay-1s">
+		    NGUYỄN CHÍ HÒA
+		    </span>
+		
+		</div>
+	</div>
+
+	<section class="section section-light">
+		<h2 class="animated rotateInUpRight delay-1s">Alexander Spatari</h2>
+		<p class="animated rotateInUpRight delay-2s">Im selfish, impatient and a little insecure. I make mistakes, I am out of control and at times hard to handle. But if you cant handle me at my worst, then you sure as hell dont deserve me at my best.
+			<br>Marilyn Monroe</p>
+	</section>
+
+	<div class="pic2">
+		<div class="text1">
+			<span class="bor tran">
+		    NEW YORK
+		  </span>
+		
+
+		</div>
+	</div>
+
+	<section class="section section-dark">
+		<h2>New york</h2>
+		<p>Youve gotta dance like there�s nobody watching,Love like youll never be hurt,Sing like theres nobody listening,And live like its heaven on earth. <br>William W. Purkey</p>
+	</section>
+
+	<div class="pic3">
+		<div class="text1">
+			<span class="bor tran">
+		    TOKYO
+		  </span>
+		
+		</div>
+	</div>
+
+	<section class="section section-dream ">
+		<h2>Tokyo</h2>
+		<p>There are only two ways to live your life. One is as though nothing is a miracle. The other is as though everything is a miracle. <br> Albert Einstein</p>
+	</section>
+</body>
+</html>
